64fb9782ae40c045d593aef7dfc345b609dea1da
Avernus54 <alchierdivino009@gmail.com>
2025-07-28 00:38:50 +0800

    Create walking.cs

---
+++ walking.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;   // Movement speed
+
+    void Update()
+    {
+        // Get input from WASD or Arrow keys
+        float horizontal = Input.GetAxis("Horizontal");  // A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical");      // W/S or Up/Down
+
+        // Create movement direction
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        // Move the player (in world space)
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+}
